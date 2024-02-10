8d0c406c2c1534a0ff41b88d0117cc59d4939fc4
csdiaries <159630328+csdiaries@users.noreply.github.com>
2024-02-10 16:54:14 +0200

    added two sum

---
+++ README.md
@@ -0,0 +1,4 @@
+# Leetcode
+
+## Arrays/Two pointers
+🟢 [01. Two Sum](https://leetcode.com/problems/two-sum/) - [solution]()
---
+++ arrays/two-sum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    dict := make(map[int]int)
+    for i, num := range nums {
+        if j, exists := dict[target - num]; exists {
+            return []int{j, i}
+        }
+        dict[num] = i
+    }
+
+    return []int{}
+}
